c2a2de5a2c94942b039b6efad80cab256922bb8c
anlopez95 <andresfeliplh@gmail.com>
2025-06-23 09:19:53 -0500

    Add files via upload
    INIT-COMMIT: Se carga origen de proyecto backend

---
+++ admisiones/src/test/java/uniandes/admisiones/AdmisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package uniandes.admisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
